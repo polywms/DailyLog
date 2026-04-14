6282a39282fb78198f744ede70bb2ddd501c4d6c
polywms <polytronmagelang@gmail.com>
2026-04-14 13:16:36 +0700

    Add app.js to cache list in service worker

--- sw.js
+++ sw.js
@@ -3,6 +3,7 @@ const urlsToCache = [
     './',
     './index.html',
     './manifest.json',
+    './app.js',
     './icon.png',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
