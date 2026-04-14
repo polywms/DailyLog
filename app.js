10abd0672f652af260fad75e72bb96f451998616
polywms <polytronmagelang@gmail.com>
2026-04-14 16:38:40 +0700

    Enhance service worker registration and update handling
    Updated service worker registration to include a version query string and added automatic refresh on controller change.

--- app.js
+++ app.js
@@ -372,6 +372,24 @@ async function jalankanSync() {
 
 window.addEventListener('online', jalankanSync);
 
-if ('serviceWorker' in navigator) { 
-    window.addEventListener('load', () => navigator.serviceWorker.register('./sw.js').catch(err => console.log('SW Error: ', err))); 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        // Tambahkan query string ?v= agar browser mengecek file sw.js yang paling baru di GitHub
+        navigator.serviceWorker.register('./sw.js?v=' + new Date().getTime())
+            .then(reg => {
+                // Paksa pengecekan update setiap kali aplikasi dibuka
+                reg.update();
+                console.log('SW terdaftar');
+            })
+            .catch(err => console.log('SW Error: ', err));
+    });
+
+
+    let refreshing = false;
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+        if (!refreshing) {
+            window.location.reload();
+            refreshing = true;
+        }
+    });
 }
